1f969e5674465e3dac8f047c351f80f7d5660bf7
Islam Sulaiman <ma.eslam@yahoo.com>
2023-07-15 20:44:47 +0300

    Tweaks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :project
+  enum status: [:pending, :in_progress, :done]
 end
