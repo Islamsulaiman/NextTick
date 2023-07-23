d54d297575a90b2f350088908f85e291bfc9ba6b
Islam Sulaiman <ma.eslam@yahoo.com>
2023-07-23 20:34:14 +0300

    Tool: adding coverage tool to check for tests covring all needed routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,15 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+
+if ENV['COV']
+  require 'simplecov'
+  SimpleCov.start('rails') do
+    add_group 'Services', 'app/services'
+  end
+end
+
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
